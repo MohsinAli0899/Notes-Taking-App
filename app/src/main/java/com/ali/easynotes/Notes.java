99e675bbc6a7be28cb8b430ed69b4c6f60076a63
Mohsin0899 <mohsin.saifi98@gmail.com>
2022-05-08 22:02:11 +0530

    First Commit

--- app/src/main/java/com/ali/easynotes/Notes.java
+++ app/src/main/java/com/ali/easynotes/Notes.java
@@ -2,8 +2,32 @@ package com.ali.easynotes;
 
 import io.realm.RealmObject;
 
-public class Notes {
+public class Notes extends RealmObject {
     String title;
     String description;
     long time;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
 }
