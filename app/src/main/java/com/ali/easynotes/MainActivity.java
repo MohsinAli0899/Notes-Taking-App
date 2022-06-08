05f04e46f98e2af522c116a73fa6b3775b025f9a
Mohsin0899 <mohsin.saifi98@gmail.com>
2022-06-08 23:39:43 +0530

    First Commit

--- app/src/main/java/com/ali/easynotes/MainActivity.java
+++ app/src/main/java/com/ali/easynotes/MainActivity.java
@@ -3,7 +3,11 @@ package com.ali.easynotes;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MaterialButton addNewNoteBtn = findViewById(R.id.addNewNoteBtn);
+
+        addNewNoteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            startActivity(new Intent(MainActivity.this, AddNoteActivity.class));
+            }
+        });
     }
 }
