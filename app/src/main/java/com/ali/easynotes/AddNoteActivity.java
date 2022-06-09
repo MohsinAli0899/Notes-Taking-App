39ff6887fe5b0f6cfe9fa07327ebeae6bd295760
Mohsin0899 <mohsin.saifi98@gmail.com>
2022-06-09 20:59:22 +0530

    First Commit

--- app/src/main/java/com/ali/easynotes/AddNoteActivity.java
+++ app/src/main/java/com/ali/easynotes/AddNoteActivity.java
@@ -3,6 +3,13 @@ package com.ali.easynotes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
+import io.realm.Realm;
 
 public class AddNoteActivity extends AppCompatActivity {
 
@@ -10,5 +17,31 @@ public class AddNoteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_note);
+
+        EditText titleInput = findViewById(R.id.titleInput);
+        EditText descriptionInput = findViewById(R.id.descriptionInput);
+        MaterialButton saveBtn = findViewById(R.id.saveBtn);
+
+        Realm.init(getApplicationContext());
+        Realm realm = Realm.getDefaultInstance();
+
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            String title = titleInput.getText().toString();
+            String description = descriptionInput.getText().toString();
+            long createdTime = System.currentTimeMillis();
+
+            realm.beginTransaction();
+            Notes note = realm.createObject(Notes.class);
+            note.setTitle(title);
+            note.setDescription(description);
+            note.setTime(createdTime);
+
+            realm.commitTransaction();
+            Toast.makeText(getApplicationContext(), "Note Saved", Toast.LENGTH_SHORT).show();
+            finish();
+            }
+        });
     }
 }
