8377e6466df85e833ced273258286aacc7b0c839
Mohsin0899 <mohsin.saifi98@gmail.com>
2022-06-09 22:14:06 +0530

    Initial Commit

--- app/src/main/java/com/ali/easynotes/MainActivity.java
+++ app/src/main/java/com/ali/easynotes/MainActivity.java
@@ -1,6 +1,8 @@
 package com.ali.easynotes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 
 import android.content.Intent;
@@ -10,6 +12,7 @@ import android.view.View;
 import com.google.android.material.button.MaterialButton;
 
 import io.realm.Realm;
+import io.realm.RealmChangeListener;
 import io.realm.RealmResults;
 
 public class MainActivity extends AppCompatActivity {
@@ -32,5 +35,17 @@ public class MainActivity extends AppCompatActivity {
         Realm realm = Realm.getDefaultInstance();
 
         RealmResults<Notes> notesList = realm.where(Notes.class).findAll();
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        MyAdapter myAdapter = new MyAdapter(getApplicationContext(),notesList);
+        recyclerView.setAdapter(myAdapter);
+
+        notesList.addChangeListener(new RealmChangeListener<RealmResults<Notes>>() {
+            @Override
+            public void onChange(RealmResults<Notes> notes) {
+                myAdapter.notifyDataSetChanged();
+            }
+        });
     }
 }
