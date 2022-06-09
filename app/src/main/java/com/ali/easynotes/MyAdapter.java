5aaf294f0cd49b22336359e6d7259d8c6b6700e4
Mohsin0899 <mohsin.saifi98@gmail.com>
2022-06-09 22:45:12 +0530

    Final Commit

--- app/src/main/java/com/ali/easynotes/MyAdapter.java
+++ app/src/main/java/com/ali/easynotes/MyAdapter.java
@@ -2,15 +2,19 @@ package com.ali.easynotes;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.text.DateFormat;
 
+import io.realm.Realm;
 import io.realm.RealmResults;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>{
@@ -37,6 +41,34 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>{
 
     String formattedTime = DateFormat.getDateTimeInstance().format(note.time);
     holder.timeOutput.setText(formattedTime);
+
+    holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+        @Override
+        public boolean onLongClick(View view) {
+
+            PopupMenu menu = new PopupMenu(context,view);
+            menu.getMenu().add("Delete");
+            menu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem menuItem) {
+
+                    if (menuItem.getTitle().equals("Delete")){
+                        //delete the note
+                        Realm realm = Realm.getDefaultInstance();
+                        realm.beginTransaction();
+                        note.deleteFromRealm();
+                        realm.commitTransaction();
+                        Toast.makeText(context, "Note Deleted", Toast.LENGTH_SHORT).show();
+                    }
+
+                    return true;
+
+                }
+            });
+            menu.show();
+            return true;
+        }
+    });
     }
 
     @Override
