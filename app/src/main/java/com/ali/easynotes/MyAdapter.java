4339e1e9d7504f126fd075a32677ad9392e1980b
Mohsin0899 <mohsin.saifi98@gmail.com>
2022-06-09 21:31:34 +0530

    First Commit

--- app/src/main/java/com/ali/easynotes/MyAdapter.java
+++ app/src/main/java/com/ali/easynotes/MyAdapter.java
@@ -1,4 +1,60 @@
 package com.ali.easynotes;
 
-public class MyAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.text.DateFormat;
+
+import io.realm.RealmResults;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>{
+
+    Context context;
+    RealmResults<Notes> notesList;
+
+    public MyAdapter(Context context, RealmResults<Notes> notesList) {
+        this.context = context;
+        this.notesList = notesList;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new MyViewHolder(LayoutInflater.from(context).inflate(R.layout.item_view,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+    Notes note = notesList.get(position);
+    holder.titleOutput.setText(note.getTitle());
+    holder.descriptionOutput.setText(note.getDescription());
+
+    String formattedTime = DateFormat.getDateTimeInstance().format(note.time);
+    holder.timeOutput.setText(formattedTime);
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        TextView titleOutput;
+        TextView descriptionOutput;
+        TextView timeOutput;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            titleOutput = itemView.findViewById(R.id.titleOutput);
+            descriptionOutput = itemView.findViewById(R.id.descriptionOutput);
+            timeOutput = itemView.findViewById(R.id.timeOutput);
+        }
+    }
 }
